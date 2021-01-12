8279f1697122e3a6d5b08f9d616461af54718b29
NinzaHost <49875415+ninzahost@users.noreply.github.com>
2021-01-12 22:12:45 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 cd ~
-git clone https://github.com/ninzahost/cpanel_clear_mail_queue
+git clone https://github.com/ninzahost/cpanel_clear_mail_queue /root/cpanel_clear_mail_queue
 cp /root/cpanel_clear_mail_queue/mail_queue_cron /etc/cron.d
 chmod 0755 /root/cpanel_clear_mail_queue/clear_queue
 rm -rf /root/cpanel_clear_mail_queue/README.md
